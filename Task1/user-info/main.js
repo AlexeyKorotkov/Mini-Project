265d0925bde6747197069be67442497161481c4f
AlexeyKorotkov <alexlera@060511@gmail.com>
2023-06-15 13:51:25 +0300

    add to miniProject 1

--- Task1/user-info/main.js
+++ Task1/user-info/main.js
@@ -1,7 +1,25 @@
-// В index.html
+// /В index.html
 // 1 отримати масив об'єктів з endpoint`а https://jsonplaceholder.typicode.com/users
 // 2 Вивести id,name всіх user в index.html. Окремий блок для кожного user.
-// 3 Додати кожному блоку кнопку/посилання , при кліку на яку відбувається перехід  на сторінку user-details.html, котра має детальну інфорацію про об'єкт на який клікнули
+// 3 Додати кожному блоку кнопку/посилання , при кліку на яку відбувається перехід  на сторінку index.html, котра має детальну інфорацію про об'єкт на який клікнули
+//
+//
+// На странице index.html:
+// 4 Вивести всю, без виключення, інформацію про об'єкт user на який клікнули
+// 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів поточного юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+//     6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку index.html, котра має детальну інфу про поточний пост.
+//
+//     На странице index.html:
+// 7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
+// 8 Нижчє інформаці про пост, вивести всі коментарі поточного поста (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+//
+// Стилизація проєкта -
+// index.html - всі блоки з user - по 2 в рядок. кнопки/аосилвння розташувати під інформацією про user.
+//     index.html - блок з інфою про user зверху сторінки. Кнопка нижчє, на 90% ширини сторінки, по центру.
+//     блоки з короткою іфною про post - в ряд по 5 .
+//     index.html - блок з інфою про пост зверху. Коментарі - по 4 в ряд.
+//     Всі елементи котрі характеризують users, posts, comments візуалізувати, так, щоб було видно що це блоки (дати фон. марджини і тд)
 // fetch('https://jsonplaceholder.typicode.com/users')
 // .then(res=>res.json())
 // .then((users) => {
@@ -37,7 +55,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
             let button = document.createElement('button');
             button.innerText = 'Info';
             button.addEventListener('click', () => {
-                window.location.href = `../user-details/index.html?userId=${user.id}`;
+                window.location.href = `../user-details/index.html`;
             });
             userBlock.appendChild(button);
             users.appendChild(userBlock);
